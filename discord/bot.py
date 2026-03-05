75e873d1e60c886e6cb4df38fca8c66b904e4a75
atsoc1993 <atsoc1993@gmail.com>
2026-03-05 18:44:43 -0500

    Basic ping working

--- discord/bot.py
+++ discord/bot.py
@@ -1,12 +1,20 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+bot_token = os.getenv('bot_token')
+assert bot_token != None, "No bot token to load from .env, create a bot in discord developer portal and get secret token"
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-bot.run('token')
+bot.run(bot_token)
